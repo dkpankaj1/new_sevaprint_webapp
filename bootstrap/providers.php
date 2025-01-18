1ca9d0e033dfa73fc75781d1d0ccc45261254f3e
kali <kali@cortexitsolution.lan>
2025-01-18 16:14:02 +0530

    fixes

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -5,6 +5,5 @@ return [
     App\Providers\GeneralSettingProvider::class,
     App\Providers\MailServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
-    App\Providers\WalletServiceProfiver::class,
     Yajra\DataTables\DataTablesServiceProvider::class,
 ];
